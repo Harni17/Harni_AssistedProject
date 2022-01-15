e83109977dc9a5ea73cbd3708f8fcefd68ce1ab3
Harni17 <97238928+Harni17@users.noreply.github.com>
2022-01-16 00:32:39 +0530

    Add files via upload

---
+++ threadCreation.java
@@ -0,0 +1,13 @@
+package com.HarniK.Assisted_Project;
+
+public class threadCreation extends Thread{
+	public static void main( String[] args){
+		threadCreation threads = new  threadCreation();
+  		threads.start();
+ 	}
+	
+	public void run(){
+  		System.out.println("thread started running..");
+  		}
+
+}
